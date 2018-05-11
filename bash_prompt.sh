9181b4bc0ffa0770d9ad491d5dc077a7e39ef6c5
Andri Danusasmita <andridsasmita@gmail.com>
2018-05-11 09:43:00 +0900

    bash prompt colors, changed author

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -1,11 +1,11 @@
 # Everyone needs a little color in their lives
-RED='\[\033[31m\]'
-GREEN='\[\033[32m\]'
-YELLOW='\[\033[33m\]'
-BLUE='\[\033[34m\]'
-PURPLE='\[\033[35m\]'
-CYAN='\[\033[36m\]'
-WHITE='\[\033[37m\]'
+RED='\[\033[91m\]'
+GREEN='\[\033[92m\]'
+YELLOW='\[\033[93m\]'
+BLUE='\[\033[94m\]'
+PURPLE='\[\033[95m\]'
+CYAN='\[\033[96m\]'
+WHITE='\[\033[97m\]'
 NIL='\[\033[00m\]'
 
 # Hostname styles
