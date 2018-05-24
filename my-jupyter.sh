e042ab2be4552a9d7bd7f4bcbfa79e47f8bbfa31
Andri Danusasmita <andridsasmita@gmail.com>
2018-05-24 04:00:16 +0000

    add python 3.6.5 installation instruction for ubuntu 16.04

---
+++ my-jupyter.sh
@@ -0,0 +1,23 @@
+# installing libraries
+echo "Installing Jupyter Notebook..."
+pip install jupyter notebook
+echo "Installing Jupyter Lab..."
+pip install jupyterlab
+# after installing jupyter notebook and jupyterlab
+echo "Generating jupyter and ipythong configs..."
+jupyter notebook --generate-config
+jupyter notebook password
+ipython profile create
+
+# add this to .jupyter/jupyter_notebook_config.py
+echo "Adding configs into .jupyter and .ipython ..."
+sed -i '1ic.NotebookApp.port = 5000' .jupyter/jupyter_notebook_config.py
+sed -i '1ic.NotebookApp.open_browser = False' .jupyter/jupyter_notebook_config.py
+sed -i '1ic.NotebookApp.ip = '0.0.0.0'' .jupyter/jupyter_notebook_config.py
+sed -i '1ic = get_config()' .jupyter/jupyter_notebook_config.py
+
+# add this to .ipython/profile_default/ipython_config.py
+sed -i '1ic.InteractiveShell.ast_node_interactivity = "all"' .ipython/profile_default/ipython_config.py
+sed -i '1ic.get_config()' .ipython/profile_default/ipython_config.py
+
+
