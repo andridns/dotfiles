4248db7f52e27714ac64fc09e3d5638d7288f792
Andri Danusasmita <andridsasmita@gmail.com>
2018-05-25 15:08:22 +0900

    Update my-conda351-linux.sh

--- my-conda351-linux.sh
+++ my-conda351-linux.sh
@@ -3,8 +3,6 @@ curl -O https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh
 sha256sum Anaconda3-5.1.0-Linux-x86_64.sh
 bash Anaconda3-5.1.0-Linux-x86_64.sh
 rm Anaconda3-5.1.0-Linux-x86_64.sh 
-sudo ln -s $HOME/anaconda3/etc/profile.d/conda.sh /etc/profile.d/conda.sh
-echo "conda activate" >> ~/.bashrc
 echo 'alias condaenv="echo $CONDA_DEFAULT_ENV"' >> ~/.bashrc
+echo ". $HOME/anaconda3/etc/profile.d/conda.sh" >> ~/.bashrc
 source $HOME/.bashrc
-
