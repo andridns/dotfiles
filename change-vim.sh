4a032e2b1492c775b8971dc178152315689c0c30
Andri Danusasmita <andridsasmita@gmail.com>
2018-08-18 21:12:29 +0900

    change-vim.sh docs

--- change-vim.sh
+++ change-vim.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# check whether xterm-clipboard is installed
 vi --version | grep xterm
+# remove vim and vim.tiny
 sudo apt-get -y remove vim vim.tiny
 sudo apt-get update
+# install full vim version
 sudo apt-get -y install vim-gtk
+# check xterm-clipboard
 vi --version | grep xterm
