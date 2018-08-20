c926f44bd4179f1d60de6035424c087b3dd2adf6
Andri Danusasmita <andridsasmita@gmail.com>
2018-08-20 13:05:15 +0900

    Fixed jupyter and vim setup configs

--- my-jupyter.sh
+++ my-jupyter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # installing libraries
 echo "Installing Jupyter Notebook..."
-sudo pip3 install jupyter notebook
+pip install jupyter notebook
 echo "Installing Jupyter Lab..."
-sudo pip3 install jupyterlab
+pip install jupyterlab
 # after installing jupyter notebook and jupyterlab
 echo "Generating jupyter and ipythong configs..."
 jupyter notebook --generate-config
@@ -11,15 +11,8 @@ jupyter notebook password
 ipython profile create
 
 # add this to .jupyter/jupyter_notebook_config.py
-echo "Adding configs into .jupyter and .ipython ..."
-sed -i "1ic.NotebookApp.port = 9000" .jupyter/jupyter_notebook_config.py
-sed -i "1ic.NotebookApp.open_browser = False" .jupyter/jupyter_notebook_config.py
-sed -i "1ic.NotebookApp.ip = '0.0.0.0'" .jupyter/jupyter_notebook_config.py
-sed -i "1ic = get_config()" .jupyter/jupyter_notebook_config.py
-
+cp ~/dotfiles/jupyter/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
 # add this to .ipython/profile_default/ipython_config.py
-sed -i "1ic.InteractiveShell.ast_node_interactivity = 'all'" .ipython/profile_default/ipython_config.py
-sed -i "1ic.get_config()" .ipython/profile_default/ipython_config.py
-# to edit back in Jupyter
-# from IPython.core.interactiveshell import InteractiveShell
-# InteractiveShell.ast_node_interactivity = 'last_expr'
+cp ~/dotfiles/ipython/profile_default/ipython_config.py ~/.ipython/profile_default/ipython_config.py
+
+
