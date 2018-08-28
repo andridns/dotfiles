a6e284ed30dbf17dc415a6e049432a8bacad1880
Andri Danusasmita <andridsasmita@gmail.com>
2018-08-28 10:23:15 +0900

    Modified vmware setup (ubuntu 18.04)

--- vmware-1804setup.sh
+++ vmware-1804setup.sh
@@ -2,10 +2,10 @@
 sudo apt update && sudo apt upgrade
 sudo apt install open-vm-tools-desktop
 # restart ubuntu
-# sudo reboot
+sudo reboot
 # test clipboard & file copypasting works
 # debugging
-hostnamectl
-systemctl status open-vm-tools.service
+# $ hostnamectl
+# $ systemctl status open-vm-tools.service
 # check that it says "Active: active (running)"
 
