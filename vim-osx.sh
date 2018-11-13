baa3fc04d843b918824e0968e612ddf454005564
Andri Danusasmita <andridsasmita@gmail.com>
2018-11-13 18:39:39 +0900

    Add vim script for OSX

---
+++ vim-osx.sh
@@ -0,0 +1,14 @@
+cd ~
+# get Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# get monokai color
+git clone https://github.com/sickill/vim-monokai
+mkdir ~/.vim/colors/
+cp vim-monokai/colors/monokai.vim ~/.vim/colors/
+rm -rf ~/vim-monokai/
+# copy vimrc to home
+cp ~/dotfiles/vimrc ~/.vimrc
+# instal vim plugins and run vim
+vi +PluginInstall +qall
+
+
