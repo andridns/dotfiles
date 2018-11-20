b9e152619ea8b410bdb599786ada70b5634a4d32
Andri Danusasmita <andridsasmita@gmail.com>
2018-11-20 20:30:02 +0900

    Git config username and email

--- recent/my-ub1604.sh
+++ recent/my-ub1604.sh
@@ -6,3 +6,6 @@ sudo apt install -y virtualenv
 cp dotfiles/bash_aliases ~/.bash_aliases
 source .bashrc
 pip install tldr youtube-dl
+git config --global user.email "andridsasmita@gmail.com"
+git config --global user.name "Andri Danusasmita"
+
