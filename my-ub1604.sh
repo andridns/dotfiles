425ef9dff470ebe49e3cd5a58e48f120582ff096
Andri Danusasmita <andridsasmita@gmail.com>
2018-11-20 20:57:37 +0900

    Add sublime text and vscode installation

--- my-ub1604.sh
+++ my-ub1604.sh
@@ -17,3 +17,20 @@ pip install tldr youtube-dl
 git config --global user.email "andridsasmita@gmail.com"
 git config --global user.name "Andri Danusasmita"
 
+# installing sublime-text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
+# installing vscode
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+l
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code
+
+
+
