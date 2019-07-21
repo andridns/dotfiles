05be798c4d3863ee9b960f1090eef753f997c09a
Andri Danusasmita <andridsasmita@gmail.com>
2019-07-21 21:01:46 +0900

    shorten youtube-dl from ydl to yd

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -4,9 +4,10 @@ alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 # for GCP
 # alias ssh_[INSTANCE]='ssh -i .ssh/[GCP_INSTANCE_NAME] [HOSTNAME]@[STATIC_IP]'
-alias ydl='youtube-dl -f 18 -i --write-auto-sub --write-sub -o "%(upload_date)s - %(title)s.%(ext)s"'
-alias ydl22='youtube-dl -f 22 -i --write-auto-sub --write-sub -o "%(upload_date)s - %(title)s.%(ext)s"'
+alias yd='youtube-dl -f 18 -i --write-auto-sub --write-sub -o "%(upload_date)s - %(title)s.%(ext)s"'
+alias yd22='youtube-dl -f 22 -i --write-auto-sub --write-sub -o "%(upload_date)s - %(title)s.%(ext)s"'
 # YDL
+alias yda='youtube-dl -f 251'
 alias l='ls -lht --color=auto'
 alias la='ls -lAh --color=auto'
 alias ll='ls -lh --color=auto'
@@ -14,11 +15,11 @@ alias ls='ls -G --color=auto'
 alias lsa='ls -lah --color=auto'
 alias lss='ls -lhS --color=auto'
 alias lsn='ls -1 | wc -l'
-alias mrblocks='cd ~/Documents/Mask_RCNN/samples/blocks/
-source ~/tf112/bin/activate
-jupyter notebook
-cd ~/Documents/Mask_RCNN/'
-
+# Sample
+#alias mrblocks='cd ~/Documents/Mask_RCNN/samples/blocks/
+#source ~/tf112/bin/activate
+#jupyter notebook
+#cd ~/Documents/Mask_RCNN/'
 alias ..="cd ../"
 alias ...="cd ../../"
 alias ....="cd ../../../"
