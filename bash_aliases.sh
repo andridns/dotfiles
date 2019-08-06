d4d43b36b6869b95fce0806dd9fce8e5f28fbafb
Andri Danusasmita <andridsasmita@gmail.com>
2019-08-07 01:37:06 +0900

    update bash aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -4,13 +4,6 @@ alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 # for GCP
 # alias ssh_[INSTANCE]='ssh -i .ssh/[GCP_INSTANCE_NAME] [HOSTNAME]@[STATIC_IP]'
-# Youtube-dl aliases
-alias ydl='youtube-dl -f'
-alias ydf='youtube-dl -F'
-alias yd='youtube-dl -f 18 -i --write-auto-sub --write-sub -o "%(upload_date)s - %(title)s.%(ext)s"'
-alias yd22='youtube-dl -f 22 -i --write-auto-sub --write-sub -o "%(upload_date)s - %(title)s.%(ext)s"'
-alias yda='youtube-dl -f 251'
-alias ydax='youtube-dl --external-downloader axel --external-downloader-args "-n 20 -a"'
 
 alias l='ls -lhtp --color=auto'
 alias la='ls -lAhp --color=auto'
@@ -29,3 +22,12 @@ alias ...="cd ../../"
 alias ....="cd ../../../"
 alias .....="cd ../../../../"
 alias ......="cd ../../../../../"
+
+
+# Youtube-dl aliases
+alias yd='youtube-dl -f 18 -i --write-auto-sub --write-sub -o "%(upload_date)s - %(title)s.%(ext)s"'
+alias yd2='youtube-dl -f 22 -i --write-auto-sub --write-sub -o "%(upload_date)s - %(title)s.%(ext)s"'
+alias yda='youtube-dl -f 251'
+alias ydax='youtube-dl --external-downloader axel --external-downloader-args "-n 20 -a"'
+alias ydf='youtube-dl -F'
+alias ydl='youtube-dl -f'
