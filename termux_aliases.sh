302bf3e23c85051dcd0e0bb267500fa16e1be9d9
Andri Danusasmita <andridsasmita@gmail.com>
2019-08-07 01:35:51 +0900

    update termux aliases

--- termux_aliases.sh
+++ termux_aliases.sh
@@ -1,9 +1,13 @@
-alias ydl='youtube-dl -f 18 -i --write-auto-sub --write-sub -o "%(upload_date)s - %(title)s.%(ext)s"'
-alias ydl22='youtube-dl -f 22 -i --write-auto-sub --write-sub -o "%(upload_date)s - %(title)s.%(ext)s"'
-alias ydlaudio='youtube-dl -f 140 -i --write-auto-sub --write-sub -o "%(upload_date)s - %(title)s.%(ext)s"'
-
 alias l='ls -lhtp --color=auto'
 alias la='ls -lAhp --color=auto'
 alias ll='ls -lhp --color=auto'
 alias lsa='ls -lahp --color=auto'
 alias lss='ls -lhSp --color=auto'
+
+
+alias yd='youtube-dl -f 18 -i --write-auto-sub --write-sub -o "%(upload_date)s - %(title)s.%(ext)s"'
+alias yd2='youtube-dl -f 22 -i --write-auto-sub --write-sub -o "%(upload_date)s - %(title)s.%(ext)s"'
+alias yda='youtube-dl -f 251'
+alias ydax='youtube-dl --external-downloader axel --external-downloader-args "-n 20 -a"'
+alias ydf='youtube-dl -F'
+alias ydl='youtube-dl -f'
