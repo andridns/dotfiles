076ec9246572043ee067c9405edce8490a234910
Andri Danusasmita <andridsasmita@gmail.com>
2019-09-06 19:18:48 +0900

    Add python 3.7.2 install instructions for ubuntu 16.04

---
+++ py372-install-u1604.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
+cd /tmp
+wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tar.xz
+tar -xf Python-3.7.2.tar.xz
+cd Python-3.7.2
+./configure --enable-optimizations
+make -j `nproc`
+sudo make altinstall
+python3.7 --version
+
+
