8a9d59a81cd029e3bb48acaf91ec28c212b7a16b
Andri Danusasmita <andridsasmita@gmail.com>
2019-09-08 21:39:14 +0900

    Update bash snippet to rename files

--- bash_loop_rename.sh
+++ bash_loop_rename.sh
@@ -1,4 +1,11 @@
-for n in $(seq -f "%02g" 1 10)
+for n in $(seq -f "%02g" 1 20)
 do 
-	mv Vikings.S03E$n.720p.EXTENDED.BluRay.x265.srt Vikings.S03E$n.1080p.10bit.BluRay.5.1.x265.HEVC-MZABI.srt
+	echo "n = $n"
+	SRC=$(ls *S04E$n*.srt | head -1)
+	DST=Vikings.S04E$n.1080p.10bit.BluRay.5.1.x265.HEVC-MZABI.srt
+	echo "Source: $SRC"
+	echo "Destination: $DST"
+	#mv *S04E$n*.srt \
+	#Vikings.S04E$n.1080p.10bit.BluRay.5.1.x265.HEVC-MZABI.srt
+	mv $SRC $DST
 done
